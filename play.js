ed37f6c0caa47e8cc0febde1c7a4778099cafbb3
maddie <lee.maddie@gmail.com>
2023-09-21 13:25:34 -0400

    Added handler for user input

--- play.js
+++ play.js
@@ -5,8 +5,18 @@ const setupInput = function() {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
   return stdin;
-}
+};
+
+const handleUserInput = function() {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
 console.log("Connecting...");
 connect();
