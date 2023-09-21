34f749201d6a3e7f249dcd45853da673f5606be7
maddie <lee.maddie@gmail.com>
2023-09-21 14:21:58 -0400

    Moved input functions into separate file

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
 const { connect } = require("./client");
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
 console.log("Connecting...");
 connect();
 setupInput();
