40fa666c7698e0eae60da11244a2a974ea17fa16
maddie <lee.maddie@gmail.com>
2023-09-22 18:47:08 -0400

    Refactored to include exit in switch-case-break

--- input.js
+++ input.js
@@ -16,11 +16,11 @@ const setupInput = (conn) => {
 
 const handleUserInput = function(key) {
   // \u0003 maps to ctrl+c input
-  if (key === "\u0003") {
-    process.exit();
-  }
-
   switch(key) {
+    case "\u0003":
+      process.exit();
+      break;
+      
     case "w":
       connection.write("Move: up");
       break;
